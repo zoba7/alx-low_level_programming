65022abb921c65e054a85153d7f52f5bf76ae324
zoba7 <themodusvivendiart@gmail.com>
2022-09-12 16:28:23 -0700

    C-variable

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,12 +8,12 @@
 int main(void)
 {
 char ch = 'a';
+
 while (ch <= 'z')
 {
 	if (ch != 'e' && ch != 'q')
 {
 	putchar(ch);
-
 }
 	ch++;
 }
--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,15 +6,15 @@
 */
 int main(void)
 {
-	int n;
+int n;
 
-	for (n = 48; n < 58; n++)
+for (n = 48; n < 58; n++)
 {
 	putchar(n);
-	if (n != 57)
+if (n != 57)
 {
-putchar(',');
-putchar(' ');
+	putchar(',');
+	putchar(' ');
 }
 
 }
