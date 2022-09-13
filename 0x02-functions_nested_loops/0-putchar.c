de6828db415383af712a02ebe373d1cf0d63132f
zoba7 <themodusvivendiart@gmail.com>
2022-09-13 12:56:54 -0700

    c-funtions

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+*main - Main function entry point
+*Return: Always 0
+*Description: print out putchar and newline
+*/
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
