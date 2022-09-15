828fc3c3b873659542cea36f1f89c1145d0edfef
zoba7 <themodusvivendiart@gmail.com>
2022-09-14 18:24:34 -0700

    Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,22 +1,13 @@
 #include "main.h"
-
 /**
 *print_alphabet - prints out alphabet in low case 10 times
-*
-*Return: void
 */
 void print_alphabet_x10(void)
 {
 	int i;
-	int letter;
 
 	for (i = 0; i <= 9; i++)
-	{
-		for (letter = 'a'; letter <= 'z'; letter++)
 		{	
-			_putchar(letter);
+			print_alphabet(letter);
 		}
-		/* new line */
-		_putchar('\n');
-	}
 }
