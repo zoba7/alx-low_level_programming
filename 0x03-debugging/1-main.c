cfe775d8854b57d8a68da7010ad9b366cbe85c39
zoba7 <themodusvivendiart@gmail.com>
2022-09-15 16:03:33 -0700

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -18,15 +18,7 @@ int main(void)
 			putchar(i)
 		*}
 		*/
-		printf("Infinite loop incoming :(\n");
 
-		i = 0;
-		/*
-		*while (i < 10)
-		*{
-			putchar(i);
-		*}
-		*/
 		printf("Infinite loop avoided! \\o/\n");
 
 		return (0);
